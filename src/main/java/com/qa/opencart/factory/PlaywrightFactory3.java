1fd3d246cbb2ca17a43db6654fd03ebadd2639ed
Reza <reza.azam@gmail.com>
2023-02-10 17:43:27 +0600

    first commit

---
+++ src/main/java/com/qa/opencart/factory/PlaywrightFactory3.java
@@ -0,0 +1,101 @@
+package com.qa.opencart.factory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class PlaywrightFactory3 {
+
+	Playwright playwright;
+	Browser browser;
+	BrowserContext browserContext;
+	Page page;
+	Properties prop;
+	
+	/**
+	private static ThreadLocal<Playwright> tlPlaywright = new ThreadLocal<>();
+	private static ThreadLocal<Browser> tlBrowser = new ThreadLocal<>();
+	private static ThreadLocal<BrowserContext> tlBrowserContext = new ThreadLocal<>();
+	private static ThreadLocal<Page> tlPage = new ThreadLocal<>();
+	
+	public static Playwright getPlaywright() {
+		return tlPlaywright.get();
+	}
+	
+	public static Browser getBrowser() {
+		return tlBrowser.get();
+	}
+	public static BrowserContext getBrowserContext() {
+		return tlBrowserContext.get();
+	}
+	public static Page getPage() {
+		return tlPage.get();
+	}
+	*/
+	public Page initBrowser(Properties prop) {
+		String browserName = prop.getProperty("browser").trim();
+		System.out.println("browser name is : " + browserName);
+		
+		playwright = Playwright.create();
+		//tlPlaywright.set(Playwright.create());
+		
+		switch(browserName.toLowerCase()) {
+		case "chromium":
+			browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			//tlBrowser.set(playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+			break;
+		case "firefox":
+			browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			//tlBrowser.set(playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+			break;
+		case "safari":
+			browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+			//tlBrowser.set(playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false)));
+			break;			
+		case "chrome":
+			browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+			//tlBrowser.set(playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false)));
+			break;
+		
+		default:
+			System.out.println("please pass the right browser name.......");
+			break;
+		}
+
+		browserContext = browser.newContext();
+		page = browserContext.newPage();
+		page.navigate(prop.getProperty("url").trim());
+		return page;
+		
+		/**
+		tlBrowserContext.set(getBrowser().newContext());
+		tlPage.set(getBrowserContext().newPage());
+		getPage().navigate(prop.getProperty("url").trim());
+		return getPage();
+		*/
+	}
+	
+	/**
+	 * this method is used to initialize the properties from config file
+	 */
+	public Properties init_prop() {
+		try {
+			FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
+			prop = new Properties();
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return prop;
+	}
+	
+}
